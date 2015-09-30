766acebdead0d84649e785b56bd25ba7dd5ca7b9
David Navarro Alvarez <davengeo@yahoo.es>
2015-09-30 11:45:11 +0200

    initial load

---
+++ src/main/java/org/davengeo/deadlocks/DeadlocksApplication.java
@@ -0,0 +1,12 @@
+package org.davengeo.deadlocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeadlocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeadlocksApplication.class, args);
+    }
+}
