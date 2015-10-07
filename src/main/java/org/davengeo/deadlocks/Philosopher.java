ae2b8f7c25e888d27823e9c351d030607b335127
David Navarro Alvarez <davengeo@yahoo.es>
2015-10-07 21:16:20 +0200

    creating the philosopher deadlock

--- src/main/java/org/davengeo/deadlocks/Philosopher.java
+++ src/main/java/org/davengeo/deadlocks/Philosopher.java
@@ -7,5 +7,20 @@ public class Philosopher {
 
     private Random random;
 
+    public Philosopher(Chopstick left, Chopstick right) {
+        this.left = left;
+        this.right = right;
+        this.random = new Random();
+    }
 
+    public void run() throws InterruptedException {
+        while(true) {
+            Thread.sleep(random.nextInt(1000));
+            synchronized (left) {
+                synchronized (right) {
+                    Thread.sleep(random.nextInt(1000));
+                }
+            }
+        }
+    }
 }
